92476f9c52cfa202323b94f97690d5a82a77b181
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-12-08 20:17:29 +0100

    Add get_mst() function

--- project1/example.py
+++ project1/example.py
@@ -1,9 +1,74 @@
 from data import runtests
 
-def my_solve(N, streets, lords):
-    print(f"Place: {N}, ulice: {len(streets)}, lordowie: {len(lords)}")
-    for a, b, t in streets:
-        pass
-    return 42
+class FindUnion:
+    def __init__(self, V) -> None:
+        self.representants = {vertex : vertex for vertex in range(1, V+1)}
+        self.levels = {vertex : 0 for vertex in range(1, V+1)}
 
-runtests(my_solve)
+    def find(self, vertex):
+        if self.representants[vertex] != vertex:
+            self.representants[vertex] = self.find(self.representants[vertex])
+        return self.representants[vertex]
+
+    def union(self, vertex1, vertex2) -> bool:
+        repr1 = self.find(vertex1)
+        repr2 = self.find(vertex2)
+
+        if repr1 == repr2:
+            return False
+
+        if self.levels[repr1] > self.levels[repr2]:
+            self.representants[repr2] = repr1
+        else:
+            self.representants[repr1] = repr2
+            if self.levels[repr1] == self.levels[repr2]:
+                self.levels[repr2] += 1
+
+        return True
+
+def get_mst(V: int, graphEdges: list[tuple[int, int, int]]) -> list[tuple[int, int, int]]:
+    """
+    Get Minimal Spanning Tree of given graph
+
+    :param V: number of verticies in graph
+    :param graphEdges: graph representation as list of edges
+    :returns: Minimal Spanning Tree graph representation as list of edges
+    """
+    unionStruct = FindUnion(V)
+
+    graphEdges.sort(key = lambda x: x[2])
+
+    mstEdges = []
+
+    # number of edges in MST
+    edgesLeft = V - 1
+
+    for vertex, neighbour, weight in graphEdges:
+        # check if current edge can be added to MST
+        if unionStruct.union(vertex, neighbour):
+            mstEdges.append((vertex, neighbour, weight))
+            edgesLeft -= 1
+            if edgesLeft == 0:
+                break
+
+    return mstEdges
+
+def solve(V: int, streets: list[tuple[int, int, int]], lords: list[int]):
+    streets = get_mst(V, streets)
+
+    print(streets)
+
+solve(6, [
+    (1, 2, 4),
+    (2, 3, 5),
+    (3, 4, 6),
+    (4, 5, 8),
+    (5, 6, 7),
+    (1, 6, 9),
+    (2, 5, 10)],
+  [
+    [1, 3],
+    [2, 5],
+    [4, 6]])
+
+# runtests(solve)
