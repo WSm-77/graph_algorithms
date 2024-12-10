cb58afd29430e177336b1585bbbde30e86c0ae26
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-12-10 09:18:36 +0100

    Clean test_lords_protection.py

--- project1/test_lords_protection.py
+++ project1/test_lords_protection.py
@@ -1,11 +1,14 @@
 import unittest
 import os
 import json
-from utils import utils
 from example import get_adjustancy_list_graph, get_mst, lords_protection
 
 class LordsProtection(unittest.TestCase):
-    def setUp(self):
+    def json_data_to_dict_graph(self, jsonDataGraph: dict[str, list[int]], convertKeys = lambda x: x, convertValues = lambda x: x):
+        return {convertKeys(key) : convertValues(value) for key, value in jsonDataGraph.items()}
+
+    def test_lords_protection(self):
+        # given
         testCasesDir = "test_cases"
         graphName = "graph1.json"
         fileName = os.path.join(testCasesDir, graphName)
@@ -16,24 +19,23 @@ class LordsProtection(unittest.TestCase):
         self.V = data["V"]
         self.streets = data["streets"]
         self.lords = data["lords"]
-        self.royalRouteEdges = data["royalRouteEdges"]
+        self.royalRouteEdges = list(map(tuple, data["royalRouteEdges"]))
+        self.verticiesProtectors = self.json_data_to_dict_graph(data["verticiesProtectors"], int)
 
         print(self.V)
         print(self.streets)
         print(self.lords)
         print(self.royalRouteEdges)
-
-    def test_lords_protection(self):
-        # given
-        exceptedVerticiesProtectors = {1: [0], 2: [0, 1], 3: [0, 1], 4: [1, 2], 5: [1, 2], 6: [2]}
+        print(self.verticiesProtectors)
 
         # when
         royalRouteEdges = get_mst(self.V, self.streets)
 
-        actualVerticiesProtectors = lords_protection(royalRouteEdges, get_adjustancy_list_graph(self.V, royalRouteEdges), self.lords)[2]
+        actualVerticiesProtectors = lords_protection(royalRouteEdges, get_adjustancy_list_graph(self.V, royalRouteEdges), self.lords)[1]
 
         # then
-        self.assertEqual(actualVerticiesProtectors, exceptedVerticiesProtectors)
+        self.assertEqual(royalRouteEdges, self.royalRouteEdges)
+        self.assertEqual(actualVerticiesProtectors, self.verticiesProtectors)
 
 if __name__ == "__main__":
     unittest.main()
